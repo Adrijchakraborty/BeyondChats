4442c11b2cecefb356d00c89040016990f030fdf
Adrijchakraborty <adrijchakraborty8@gmail.com>
2025-02-02 21:41:25 +0530

    file change

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,10 @@ const LazyMainScreen = React.lazy(()=> import ("./MainScreen"));
 const LazySetupOrg = React.lazy(()=> import ("./SetupOrg"));
 const LazyLogin = React.lazy(()=> import ("./Login"));
 const LazyRegister = React.lazy(()=> import ("./Register"));
-const LazyTestIntegration = React.lazy(()=> import ("./TestIntegration"));
 
 export {
     LazyMainScreen,
     LazyLogin,
     LazyRegister,
-    LazyTestIntegration,
     LazySetupOrg
 }
